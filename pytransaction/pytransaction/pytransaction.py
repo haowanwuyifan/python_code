6bb77cebb61302e3ec24752913bf961c6fa666d3
一凡 吴 <823348523@qq.com>
2018-05-25 19:55:19 +0800

    sss

---
+++ pytransaction/pytransaction/pytransaction.py
@@ -0,0 +1,9 @@
+﻿import urllib.request
+url="http://www.baidu.com"
+headers=("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36")
+opener=urllib.request.build_opener()
+opener.addheaders=[headers]
+data=opener.open(url).read()
+fhandle=open("D:/test/3.html","wb")
+fhandle.write(data)
+fhandle.close()
